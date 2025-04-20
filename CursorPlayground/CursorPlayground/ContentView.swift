9689530f4e404544d193a3d06805fc5ea38f0530
Anton Breza <antonrxdt@gmail.com>
2025-04-20 09:29:57 +0200

    cleaned up the project

---
+++ CursorPlayground/CursorPlayground/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  CursorPlayground
+//
+//  Created by Anton Breza on 08.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        EmptyView()
+    }
+}
---
+++ CursorPlayground/CursorPlayground/CursorPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursorPlaygroundApp.swift
+//  CursorPlayground
+//
+//  Created by Anton Breza on 08.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct CursorPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
